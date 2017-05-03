92b3f12b466c10bba9fb57234d956d8485daa914
nicky evers <livelyevers@gmail.com>
2017-05-03 10:58:24 -0700

    roundtrip and get all e2e tests working for props

---
+++ test/e2e/props.test.js
@@ -0,0 +1,70 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('prop API', () => {
+
+    before(db.drop);
+
+    it('returns empty array from get all', () => {
+        return request.get('/props')
+            .then(res => {
+                const props = res.body;
+                assert.deepEqual(props, []);
+            });
+    });
+
+    let poi = {
+        name: 'poi',
+        number: 2,
+        hard: true
+    };
+
+    let club = {
+        name: 'club',
+        number: 3,
+        hard: true
+    };
+
+    function saveProp(prop) {
+        return request
+            .post('/props')
+            .send(prop)
+            .then(res => res.body);
+    }
+
+    it('roundtrips a prop', () => {
+        return saveProp(poi)
+            .then(saved => {
+                assert.ok(saved._id);
+                poi = saved;
+            })
+            .then(() => request.get(`/props/${poi._id}`))
+            .then(res => res.body)
+            .then(got => {
+                assert.equal(got._id, poi._id);
+            });
+    });
+
+    it('gets all props', () => {
+        return saveProp(club)
+            .then(saved => club = saved)
+            .then(() => request.get('/props'))
+            .then(res => res.body)
+            .then(props => {
+                assert.equal(props.length, 2);
+                assert.include(props, {
+                    _id: poi._id,
+                    name: poi.name,
+                    number: poi.number,
+                    hard: poi.hard
+                });
+                assert.include(props, {
+                    _id: club._id,
+                    name: club.name,
+                    number: club.number,
+                    hard: club.hard
+                });
+            });
+    });
+});
